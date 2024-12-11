161e5e29a8abeb0eb39e871bdf9094f815ca6680
viapulsa <viapulsa@viapulsas-MBP.lan>
2024-12-11 15:39:21 +0700

    reusable component timeline dot

--- src/components/organism/Timeline.jsx
+++ src/components/organism/Timeline.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import TitleSection from "../atoms/TitleSection";
+import TimelineDotUI from "../atoms/TimelineDotUI";
 
 const Timeline = () => {
    return (
@@ -7,6 +8,10 @@ const Timeline = () => {
          <TitleSection />
          <div className="flex flex-row min-h-96 border border-solid border-white border-x-0 bg-[url('/map.jpg')] bg-cover bg-center w-screen relative ">
             <div className="absolute inset-0 bg-black bg-opacity-50 backdrop-blur-md"></div>
+            <div className="relative z-10">
+               <TimelineDotUI active={true} />
+               <TimelineDotUI />
+            </div>
          </div>
       </section>
    );
